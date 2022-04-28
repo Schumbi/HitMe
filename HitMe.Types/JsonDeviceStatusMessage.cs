d43be396548adabbcc4dada4cd2c59ec26a5f142
Ralf Warmuth <mightynullptr@schumbi.de>
2022-04-28 15:08:27 +0200

    Add some sane defaults for device config.

--- HitMe.Types/JsonDeviceStatusMessage.cs
+++ HitMe.Types/JsonDeviceStatusMessage.cs
@@ -13,23 +13,23 @@
     public class JsonDeviceStatusMessage
     {
         [JsonPropertyName("type")]
-        public MessageType Type { get; set; }
+        public MessageType Type { get; set; } = MessageType.PARSEERR;
 
         [JsonPropertyName("readable")]
-        public bool Readable { get; set; }
+        public bool Readable { get; set; } = false;
 
         [JsonPropertyName("range")]
-        public BMA020Range Range { get; set; }
+        public BMA020Range Range { get; set; } = BMA020Range.BMA020_RANGE_2G;
 
         [JsonPropertyName("bandwidth")]
-        public BMA020BANDWIDTH Bandwidth { get; set; }
+        public BMA020BANDWIDTH Bandwidth { get; set; } = BMA020BANDWIDTH.BMA020_BW_25HZ;
 
         [JsonPropertyName("millis")]
-        public int TimeStamp_ms { get; set; }
+        public int TimeStamp_ms { get; set; } = 0;
 
         public static JsonDeviceStatusMessage FromJson(string jsonString)
         {
-            JsonDeviceStatusMessage message = new JsonDeviceStatusMessage();
+            JsonDeviceStatusMessage message = new ();
             try {
                 message = JsonSerializer.Deserialize<JsonDeviceStatusMessage>(jsonString) ?? new JsonDeviceStatusMessage();
             }
